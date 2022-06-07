dfd65e4047476585451cc7f7326083332f107dd5
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-06-07 18:57:02 +0530

    Create Merge Overlapping Subintervals .cpp

---
+++ Merge Overlapping Subintervals .cpp
@@ -0,0 +1,26 @@
+vector<vector<int>> mergeIntervals(vector<vector<int>> &intervals)
+{
+    //no need to use any additional data-structures like Stack.
+        vector<vector<int>> mergedIntervals;
+        
+        if(intervals.size()==0){
+            return mergedIntervals;
+        }
+        
+        sort(intervals.begin(),intervals.end());
+        vector<int> tempInterval=intervals[0];
+        
+        for(auto it:intervals){
+            if(tempInterval[1] >= it[0]){
+                tempInterval[1] = max(tempInterval[1],it[1]);
+            }
+            else{
+                mergedIntervals.push_back(tempInterval);
+                tempInterval=it;
+            }
+        }
+        mergedIntervals.push_back(tempInterval);
+        return mergedIntervals;
+}
+// Time : O(nlogn + n) for Sorting & traversing
+// Space : O(n) to store results.
