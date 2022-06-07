97dd8a15e52ce2e946e820808f353961a28cdbcf
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-06-07 21:12:26 +0530

    Update Search in a 2d Matrix.cpp

--- Search in a 2d Matrix.cpp
+++ Search in a 2d Matrix.cpp
@@ -1,6 +1,6 @@
-class Solution {
-public:
-    bool _binary_search(vector<vector<int>>& matrix,int low,int high,int target){
+#include <bits/stdc++.h> 
+#include <vector>
+bool _binary_search(vector<vector<int>>& matrix,int low,int high,int target){
         
         if(low>high) return false;
         
@@ -12,10 +12,8 @@ public:
         else if(target < matrix[row][column]) return _binary_search(matrix,low,mid-1,target);
         else return _binary_search(matrix,mid+1,high,target);
     }
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size() * matrix[0].size();
-        return _binary_search(matrix,0,n-1,target);
-    }
-};
+bool findTargetInMatrix(vector < vector < int >> & matrix, int m, int n, int target) {
+    return _binary_search(matrix,0,m*n-1,target);
+}
 
 // Time Compl: O(log(m*n)) Sapce: O(1)
