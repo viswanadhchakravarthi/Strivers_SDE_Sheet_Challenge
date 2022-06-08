83e7b71fc11bf2db7c245209aa19ea69d9225c0b
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-06-08 20:37:31 +0530

    Update Modular Exponentiation.cpp

--- Modular Exponentiation.cpp
+++ Modular Exponentiation.cpp
@@ -1,1 +1,18 @@
-
+int modularExponentiation(int x, int n, int m) {
+	// Range of Int : -2,147,483,648 to 2,147,483,647
+    long long ans = 1;
+    x = x % m;
+    // Edge Case bcz if (-2,147,483,648*-1) stored in INT , it gets overflown.
+    // So store in Long Long
+    while( n > 0){
+        if( n % 2 == 0){
+            x = (1LL * x * x) % m;
+            n = n >> 1;
+        }   
+        else{
+            ans = (1LL * ans * x ) % m;
+            n = n - 1;
+        }
+    }
+    return (int)ans;
+}
