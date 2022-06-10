5a405638337e4f40f4b43bf3f3d25aa2ee285e64
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-06-11 01:43:34 +0530

    Create Detect a cycle in Linked List.cpp

---
+++ Detect a cycle in Linked List.cpp
@@ -0,0 +1,17 @@
+bool detectCycle(Node *head)
+{
+	if(head==NULL)  // here not check for head->nxt bcz at while loop anyhow we're checking 
+            return false;
+        
+        Node *slow=head,*fast=head;
+        
+        while(fast->next && fast->next->next){ 
+            // for 1st iteration, until now we've checked if head,head->nxt,head->nxt->nxt is NULL
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) 
+                return true;
+        }
+        return false;
+}
+// time: O(n), space: O(1)
