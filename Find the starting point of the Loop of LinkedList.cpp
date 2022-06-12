db04a88c06bcaa680a75ef2a961030d015709572
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-06-13 00:31:30 +0530

    Create Find the starting point of the Loop of LinkedList.cpp

---
+++ Find the starting point of the Loop of LinkedList.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+Node *firstNode(Node *head)
+{
+	if(head==NULL)  // here not check for head->nxt bcz at while loop anyhow we're checking 
+            return head;
+        
+        Node *slow=head,*fast=head,*entry=head;
+        
+        while(fast->next && fast->next->next){ 
+            // for 1st iteration, until now we've checked if head,head->nxt,head->nxt->nxt is NULL
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                while(entry!=slow){
+                    entry=entry->next;
+                    slow=slow->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+}
