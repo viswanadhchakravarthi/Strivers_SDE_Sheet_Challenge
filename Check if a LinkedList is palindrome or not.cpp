5041e17f6e4018e87ab217a039606f0c6971cbc1
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-06-13 00:27:07 +0530

    Create Check if a LinkedList is palindrome or not.cpp

---
+++ Check if a LinkedList is palindrome or not.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+LinkedListNode<int> * reverseList(LinkedListNode<int> * head) {
+        LinkedListNode<int> *newHead=NULL,*nxt;
+        while(head!=NULL){
+            // saving next node for reference
+            nxt=head->next;
+            // breaking old link, and adding a new link
+            head->next=newHead;
+            // shift all ptr's to repeat the above steps
+            newHead=head;
+            head=nxt;
+        }
+        return newHead; // bcz dummy pointing to last element
+    }
+bool isPalindrome(LinkedListNode<int> *head) {
+    if(head==NULL || head->next==NULL)
+            return true;
+        LinkedListNode<int> *slow=head,*fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+                slow=slow->next;
+                fast=fast->next->next;
+        }
+        slow->next=reverseList(slow->next);
+        
+        LinkedListNode<int> *tmp=head;
+        slow=slow->next;
+        while(slow!=NULL){
+            if(slow->data != tmp->data) 
+                return false;
+            slow=slow->next;
+            tmp=tmp->next;
+        }
+        return true;
+}
