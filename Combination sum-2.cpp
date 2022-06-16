5a84d07c20807f3fa7b7b646b8a228c642abee20
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-06-17 01:45:24 +0530

    Update Combination sum-2.cpp

--- Combination sum-2.cpp
+++ Combination sum-2.cpp
@@ -1,21 +1,23 @@
 #include<algorithm>
 void find_uniq_comb(vector<int>& arr,int itr,int target,vector<vector<int>>& res,vector<int>& ds)
 {
-    if(target==0)
-    {
-        res.push_back(ds);
-        return;
-    }
-    if(itr<0)
-        return;
-    for(int i=itr; i>=0; i--)
-    {
-        if(i!=itr && arr[i]==arr[i+1])
-            continue;
-        ds.push_back(arr[i]);
-        find_uniq_comb(arr,i-1,target-arr[i],res,ds);
-        ds.pop_back();
-    }
+if(target==0)
+{
+    res.push_back(ds);
+    return;
+}
+if(itr<0)
+    return;
+for(int i=itr; i>=0; i--)
+{
+    if(i!=itr && arr[i]==arr[i+1])
+	continue;
+    if(target<arr[i])
+	break;
+    ds.push_back(arr[i]);
+    find_uniq_comb(arr,i-1,target-arr[i],res,ds);
+    ds.pop_back();
+}
 }
 vector<vector<int>> combinationSum2(vector<int> &arr, int n, int target)
 {
