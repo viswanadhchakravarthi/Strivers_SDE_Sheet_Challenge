e293a27771df7cca59767b2346b3062b5b9375b2
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-06-17 16:41:15 +0530

    Create Palindrome Partitioning.cpp

---
+++ Palindrome Partitioning.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+bool isPalindrome(string &s,int start,int end)
+{
+    while(start<=end)
+    {
+        if(s[start++]!=s[end--])
+            return false;
+    }
+    return true;
+}
+
+void palin_Partition(string &s,int ind,vector<vector<string>> &res,vector<string> &ds)
+{
+    if(ind==s.size())
+    {   
+        res.push_back(ds);
+        return;
+    }
+    for(int i=ind; i<s.size(); i++)
+    {
+        if(isPalindrome(s,ind,i))
+        {
+            ds.push_back(s.substr(ind,i-ind+1));
+            palin_Partition(s,i+1,res,ds);
+            ds.pop_back();
+        }
+    }
+}
+
+vector<vector<string>> partition(string &s) 
+{
+    vector<vector<string>> res;
+    vector<string> ds;
+    palin_Partition(s,0,res,ds);
+    return res;
+}
+
+    
+    
