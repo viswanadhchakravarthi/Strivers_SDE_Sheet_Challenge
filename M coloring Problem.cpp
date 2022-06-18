2e5cf4b17258f71c75d7e79f35b493b87fa324ea
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-06-19 02:08:07 +0530

    Update M coloring Problem.cpp

--- M coloring Problem.cpp
+++ M coloring Problem.cpp
@@ -2,39 +2,37 @@ bool possible(vector<vector<int>> &mat, int color,int nodesColors[],int n,int no
 {
     for(int adj_node=0; adj_node<n; adj_node++)
     {
-        if(mat[node][adj_node]==1 && nodesColors[adj_node]==color)
+        if( adj_node!=node && mat[node][adj_node]==1 && nodesColors[adj_node]==color)
         {
             return false;
         }
     }
     return true;
 }
-bool solve(vector<vector<int>> &mat, int m,int nodesColors[],int n)
+bool solve(int node,vector<vector<int>> &mat, int m,int nodesColors[],int n)
 {
-    for(int node=0; node<n; node++)
+    if(node==n)
+        return true; 
+    for(int color=1; color<=m; color++)
     {
-        if(nodesColors[node]==0)
+        if(possible(mat,color,nodesColors,n,node))
         {
-            for(int color=1; color<=m; color++)
-            {
-                if(possible(mat,color,nodesColors,n,node))
-                {
-                    nodesColors[node] = color;
-                    if(solve(mat,m,nodesColors,n))
-                        return true;
-                    nodesColors[node] = 0;
-                }
-            }
-            return false;
+            nodesColors[node] = color;
+            if(solve(node+1,mat,m,nodesColors,n))
+                return true;
+            nodesColors[node] = 0;
         }
     }
-    return true;
+    return false;
 }
 string graphColoring(vector<vector<int>> &mat, int m) {
     int n = mat.size();
     int nodesColors[n]={0};
-    if(solve(mat,m,nodesColors,n))
+    if(solve(0,mat,m,nodesColors,n))
         return "YES";
     else
         return "NO";
 }
+
+// Time: O(n^m) where n is #nodes && m is #colors
+// Space: O(n) bcz at max the depth of the stack is #nodes
