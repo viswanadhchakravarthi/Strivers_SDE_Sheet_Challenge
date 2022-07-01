2b236b3d0d673537acdf8aa81e751eef77504fd7
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-07-01 16:52:11 +0530

    Create Stock Span Problem.cpp

---
+++ Stock Span Problem.cpp
@@ -0,0 +1,24 @@
+#include<stack>
+vector<int> findSpans(vector<int> &price) {
+    // Write your code here.
+    int n = price.size();
+    vector<int> res(n);
+    vector<pair<int,int>> nge(n,{-1,-1});
+    stack<pair<int,int>> s;
+    
+    for(int i=0; i<n; i++)
+    {
+        while(!s.empty() && (s.top()).first <= price[i])
+            s.pop();
+        if(!s.empty())
+            nge[i]=s.top();
+        else
+            nge[i]={-1,-1};
+        s.push({price[i],i});
+    }
+    for(int i=0; i<n; i++)
+    {
+        res[i] =  i - nge[i].second;
+    }
+    return res;
+}
