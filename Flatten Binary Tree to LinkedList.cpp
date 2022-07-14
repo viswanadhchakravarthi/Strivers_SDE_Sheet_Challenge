d347470a06fd5c896f42c77c4c3ff34b5a8a5969
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-07-14 11:55:00 +0530

    Update Flatten Binary Tree to LinkedList.cpp

--- Flatten Binary Tree to LinkedList.cpp
+++ Flatten Binary Tree to LinkedList.cpp
@@ -1,19 +1,43 @@
 class Recursive // Time: O(N) & Space: O(N)
 {
-  void flatten(TreeNode<int> *root,TreeNode<int>* &prev)
-  {
-      if(root==NULL)
-          return;
-      flatten(root->right,prev);
-      flatten(root->left,prev);
-      root -> right = prev;
-      root -> left = NULL;
-      prev = root;
-  }
-  TreeNode<int> *flattenBinaryTree(TreeNode<int> *root)
-  {
-      TreeNode<int>* prev=NULL;
-      flatten(root,prev);
-      return prev;
-  }
+  public:
+      void flatten(TreeNode<int> *root,TreeNode<int>* &prev)
+      {
+          if(root==NULL)
+              return;
+          flatten(root->right,prev);
+          flatten(root->left,prev);
+          root -> right = prev;
+          root -> left = NULL;
+          prev = root;
+      }
+      TreeNode<int> *flattenBinaryTree(TreeNode<int> *root)
+      {
+          TreeNode<int>* prev=NULL;
+          flatten(root,prev);
+          return prev;
+      }
+};
+
+class Iterative // Time: O(N) , Space: O(N)
+{
+  public:
+    TreeNode<int> *flattenBinaryTree(TreeNode<int> *root)
+    {
+        stack<TreeNode<int>*> st;
+        st.push(root);
+        while(!st.empty())
+        {
+            TreeNode<int> *curr = st.top();
+            st.pop();
+            if(curr->right)
+                st.push(curr->right);
+            if(curr->left)
+                st.push(curr->left);
+            if(!st.empty())
+                curr -> right = st.top();
+            curr -> left = NULL;
+        }
+        return root;
+    }
 };
