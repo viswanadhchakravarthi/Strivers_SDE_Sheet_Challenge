1962e6bd8d6a956ecc6a0a54577ce34b43b09a51
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-07-14 13:04:04 +0530

    Create Check for Children Sum Property.cpp

---
+++ Check for Children Sum Property.cpp
@@ -0,0 +1,24 @@
+void changeTree(BinaryTreeNode < int > * root) { // Time: O(N) , Space:O(N)
+    if(root==NULL)
+        return;
+    int sum=0;
+    if(root->left) 
+      sum += root->left->data;
+    if(root->right) 
+      sum +=  root->right->data;
+    if(sum < root->data)
+    {
+        if(root->left) root->left->data = root->data;
+        if(root->right) root->right->data = root->data;
+    }
+    else{ // This part can be ignored
+        root->data = sum;
+    }
+    changeTree(root->left);
+    changeTree(root->right);
+    int total=0;
+    if(root->left) total += root->left->data;
+    if(root->right) total += root->right->data;
+    if(root->left || root->right) // make sure there is some child below the root, or else it would end up making curr node data as zero
+        root->data = total;
+}  
