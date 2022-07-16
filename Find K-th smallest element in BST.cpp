f4c388820ef869a3261095f338f33a47cab9fb79
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-07-16 17:09:13 +0530

    Create Find K-th smallest element in BST.cpp

---
+++ Find K-th smallest element in BST.cpp
@@ -0,0 +1,20 @@
+void inOrder(TreeNode<int> *root,int &k,int &ans)
+{
+    if(!root) return;
+    inOrder(root->left,k,ans);
+    k--;
+    if(k==0)
+    {
+      ans = root->data;
+      return;
+    }
+    inOrder(root->right,k,ans);
+}
+int kthSmallest(TreeNode<int> *root, int k)
+{
+    int ans=0;
+    inOrder(root,k,ans);
+    return ans;
+}
+
+// Time: O(N) , Space: O(N) space can be avoided using morris inoder traversal
