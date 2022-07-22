313d3c7611137b6b96b369d1f2c22d67c4eb992a
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-07-22 12:30:10 +0530

    Update Flood-fill Algorithm.cpp

--- Flood-fill Algorithm.cpp
+++ Flood-fill Algorithm.cpp
@@ -13,7 +13,8 @@ void helper(vector<vector<int>> &image, int x, int y, int oldColor, int newColor
 vector<vector<int>> floodFill(vector<vector<int>> &image, int x, int y, int newColor)
 {
     int oldColor = image[x][y];
-    // this edge is v.imp to handle bcz if not added then non-neighbour oldColor will also be painted
+    // this edge is v.imp to handle bcz if not added then non-neighbour oldColor will also be painted & 
+    // if not added , stack overFlow may occurs bcz of infinite recursion-> Runtime error
     if(oldColor == newColor) // why to paint again, just return
         return image;
     helper(image,x,y,oldColor,newColor);
