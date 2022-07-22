cdb3c1ea209fbcb42e3f3f61b94efd9726393105
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-07-22 12:26:17 +0530

    Create Flood-fill Algorithm.cpp

---
+++ Flood-fill Algorithm.cpp
@@ -0,0 +1,21 @@
+void helper(vector<vector<int>> &image, int x, int y, int oldColor, int newColor)
+{
+    if(x<0 || y<0 || x>=image.size() || y>=image[0].size())
+        return;
+    if(image[x][y]!=oldColor )
+        return;
+    image[x][y]=newColor;
+    helper(image,x-1,y,oldColor,newColor);
+    helper(image,x+1,y,oldColor,newColor);
+    helper(image,x,y-1,oldColor,newColor);
+    helper(image,x,y+1,oldColor,newColor);
+}
+vector<vector<int>> floodFill(vector<vector<int>> &image, int x, int y, int newColor)
+{
+    int oldColor = image[x][y];
+    // this edge is v.imp to handle bcz if not added then non-neighbour oldColor will also be painted
+    if(oldColor == newColor) // why to paint again, just return
+        return image;
+    helper(image,x,y,oldColor,newColor);
+    return image;
+}
