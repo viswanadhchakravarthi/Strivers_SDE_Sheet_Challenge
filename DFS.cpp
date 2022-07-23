15f6fa757c526d9ce3e3de46b06297afb747d11a
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-07-23 12:44:01 +0530

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,48 +1,90 @@
-class GraphNode
+class Solution1 // good approach for solving problems bcz easy to write
 {
-    public:
-    int data;
-    bool visited;
-    vector<GraphNode*> neighbours;
-    GraphNode(int data)
+    void dfs(int node, vector<int> adjList[], vector<bool> &vis,vector<int> &storeDFS)
     {
-        visited = false;
-        this->data = data;
+        if(vis[node]) return;
+        storeDFS.push_back(node);
+        vis[node] = true;
+        for(auto nn : adjList[node]) // nn : neighbour_node
+            dfs(nn,adjList,vis,storeDFS);
+    }
+  public:
+    vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
+    {
+        vector<int> adjList[V]; // Graph_representation
+        for(int j=0; j<E; j++)
+        {
+            // edges[j][0] - node1, edges[j][1] - node2
+            int node1 = edges[j][0], node2 = edges[j][1];
+            adjList[node1].push_back(node2);
+            adjList[node2].push_back(node1);
+        }
+        vector<vector<int>> components;
+        vector<bool> vis(V+1,false);
+        for(int i=0; i<V; i++)
+        {
+            vector<int> storeDFS;
+            if(vis[i]==false)
+            {
+                dfs(i,adjList,vis,storeDFS);
+                sort(storeDFS.begin(),storeDFS.end());
+                components.push_back(storeDFS);
+            }
+        }
+        return components; // lists of components
     }
 };
-void dfs(GraphNode* node,vector<int> &v)
+
+class Solution2 // good but lengthy, not recommended to use in problems , unless there is no other choice
 {
-    if(node->visited) return;
-    v.push_back(node->data);
-    node->visited = true;
-    for(auto neighbour : node->neighbours)
-        dfs(neighbour,v);
-}
-vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
-{
-    unordered_map<int,GraphNode*> um;
-    for(int i=0; i<V; i++)
-        um[i] = new GraphNode(i);
-    for(int j=0; j<E; j++)
-    {
-        // edges[j][0] -> node1_val, edges[j][1] -> node2_val
-        auto node1 = um[edges[j][0]], node2 = um[edges[j][1]];
-        vector<GraphNode*> &n1n = node1->neighbours; // node1's neighbour[n1n]
-        vector<GraphNode*> &n2n = node2->neighbours; // node2's neighbour[n2n]
-        n1n.push_back(node2);
-        n2n.push_back(node1);
-    }
-    vector<vector<int>> res;
-    for(int i=0; i<V; i++)
-    {
-        vector<int> component;
-        GraphNode *curr = um[i];
-        if(!curr->visited)
+    private:
+        class GraphNode
         {
-            dfs(curr,component);
-            sort(component.begin(),component.end());
-            res.push_back(component);
+            public:
+            int data;
+            bool visited;
+            vector<GraphNode*> neighbours;
+            GraphNode(int data)
+            {
+                visited = false;
+                this->data = data;
+            }
+        };
+        void dfs(GraphNode* node,vector<int> &v)
+        {
+            if(node->visited) return;
+            v.push_back(node->data);
+            node->visited = true;
+            for(auto neighbour : node->neighbours)
+                dfs(neighbour,v);
         }
-    }
-    return res; // lists of components
-}
+    public:        
+        vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
+        {
+            unordered_map<int,GraphNode*> um;
+            for(int i=0; i<V; i++)
+                um[i] = new GraphNode(i);
+            for(int j=0; j<E; j++)
+            {
+                // edges[j][0] -> node1_val, edges[j][1] -> node2_val
+                auto node1 = um[edges[j][0]], node2 = um[edges[j][1]];
+                vector<GraphNode*> &n1n = node1->neighbours; // node1's neighbour[n1n]
+                vector<GraphNode*> &n2n = node2->neighbours; // node2's neighbour[n2n]
+                n1n.push_back(node2);
+                n2n.push_back(node1);
+            }
+            vector<vector<int>> res;
+            for(int i=0; i<V; i++)
+            {
+                vector<int> component;
+                GraphNode *curr = um[i];
+                if(!curr->visited)
+                {
+                    dfs(curr,component);
+                    sort(component.begin(),component.end());
+                    res.push_back(component);
+                }
+            }
+            return res; // lists of components
+        }  
+};
