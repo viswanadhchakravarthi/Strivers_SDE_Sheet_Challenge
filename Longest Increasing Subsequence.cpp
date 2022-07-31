395fb856323aaebc8c484a3c9c7c87c1a4b0ab4a
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-07-31 19:12:54 +0530

    mind blew 🤯

--- Longest Increasing Subsequence.cpp
+++ Longest Increasing Subsequence.cpp
@@ -9,13 +9,18 @@ int longestIncreasingSubsequence(int arr[], int n)
         if(arr[i]>temp.back()){
             temp.push_back(arr[i]);
             len++;
+            // this case is self explanatory
         }
         else{
             int ind = lower_bound(temp.begin(),temp.end(),arr[i])-temp.begin(); 
             temp[ind] = arr[i];
+            // this case is tricky, multiple conditions are hidden here
+            // do a dry run to get clarity
+            // anyhow we'll get max LIS length, but not LIS
         }
     }
     return len;
 }
+
 // Time: (n * log n)
 // space: O(n)
